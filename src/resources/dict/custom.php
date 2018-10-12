71f96ca217323672c358ecdf660c6869a9342ba0
Ando Roots <ando@sqroot.eu>
2018-10-12 21:05:00 +0300

    Add custom dict

---
+++ src/resources/dict/custom.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'comprehensive documentation',
+    'distributed cloud infrastructure',
+    'give a talk',
+    'giving a keynote',
+    'key visions',
+    'knowledge sharing',
+    'new heights',
+    'next steps',
+    'out of box thinking',
+    'outside the box thinking',
+    'scalable services',
+    'to be involved in',
+    'world class',
+];
